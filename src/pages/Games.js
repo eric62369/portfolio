ad54448a9d4a4ade42f5ac4264ae4d1bbfb349b0
Eric Yoon <eyoon525@gmail.com>
2024-01-10 15:13:00 -0800

    save package.json scripts

---
+++ src/pages/Games.js
@@ -0,0 +1,22 @@
+
+export default function Games() {
+    return (
+      <div className="Games">
+        Projects:
+        Personally hosted player matchmaking server
+        Technologies used: Docker
+        Engineering tradeoffs:
+        Specifications mean - simple functions at first, no database for player leaderboards (unnecessary feature)
+  
+        
+        In progress: 2D Fighting game: private github repo 
+  
+        Sidescrolling beat-em-up prototype https://github.com/eric62369/AFGBeatemup
+  
+        python plays cave story prototype https://github.com/eric62369/python-plays-cave-story
+  
+        Android phone alarm app https://github.com/eric62369/gradual-alarm
+  
+        Android note taking app https://github.com/eric62369/ScholarGrind-android
+      </div>
+    )}
